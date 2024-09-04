87ab8ef8c2ff34a79b59e57048eab09b94ac92e0
roman0425 <romanmateiuk@outlook.com>
2024-09-04 15:49:27 +0700

    remove redirection for testing

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
   const router = useRouter();
   const { data: session, status } = useSession();
 
-  if (status === "unauthenticated") {
-    router.push("/auth/signin");
-  }
+  // if (status === "unauthenticated") {
+  //   router.push("/auth/signin");
+  // }
 
   const dropdownOptions = [
     {
