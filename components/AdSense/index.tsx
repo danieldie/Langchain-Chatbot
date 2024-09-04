7661fea461c00c47adf60da3dcd11a95218c7274
roman0425 <romanmateiuk@outlook.com>
2024-09-04 14:42:01 +0700

    remove ad size

--- components/AdSense/index.tsx
+++ components/AdSense/index.tsx
@@ -51,8 +51,6 @@ const AdSense = (props: AdsBannerProps) => {
      style={{
       display: "block",
       overflow: "hidden",
-      width: "728px",
-      height: "250px",
       border: process.env.NODE_ENV === "development" ? "1px solid red" : "none",
      }}
     //  data-adtest="on"
